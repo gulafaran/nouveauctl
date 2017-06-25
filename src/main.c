9f770e08e46156adceb6d8cd9213ecf4699fd0c7
Tom Englund <tomenglund26@gmail.com>
2017-06-25 17:41:28 +0200

    complete rewrite, added --get-temp

--- src/main.c
+++ src/main.c
@@ -20,8 +20,30 @@
 
 #define pbufsize 40
 #define DRI_PATH "/sys/kernel/debug/dri/"
-#define BOOST_FILE "boost"
-#define PSTATE_FILE "pstate"
+#define BOOST_FILE "/boost"
+#define PSTATE_FILE "/pstate"
+#define HWMON_PATH "/sys/class/hwmon/"
+#define HWMON_TEMP_FILE "/temp1_input"
+
+struct nouveau {
+  char *dri_path;
+  char *hwmon_path;
+  char *pstate_file;
+  char *boost_file;
+  int gpu_temp;
+  char *current_clocks;
+};
+
+struct optargs {
+  char *pstate;
+  char *boost;
+  char *card;
+  int printclocks;
+  int printpstate;
+  int printboost;
+  int printcards;
+  int printtemp;
+};
 
 void safefree(void **pp) {
   assert(pp);  /* in debug mode abort if pp is NULL */
@@ -39,18 +61,6 @@ char *combine(char *str1, char *str2) {
   strcat(str, str2);
 
   return str;
-}
-
-char *get_path(char *file, char *card) {
-  size_t l = (strlen(file) + strlen(card) + strlen(DRI_PATH) + 2);
-  char *buffer = malloc(l);
-
-  strcpy(buffer, DRI_PATH);
-  strcat(buffer, card);
-  strcat(buffer, "/");
-  strcat(buffer, file);
-
-  return buffer;
 }
 
 char *get_cards(int printcards) {
@@ -99,17 +109,152 @@ char *get_cards(int printcards) {
     fprintf(stderr, "Couldnt open the directory : %s \n", DRI_PATH);
   }
 
+  if(card == NULL)
+    card = "Unknown";
+
   return card;
 }
 
-void print_file(char *file, char *card) {
-  char *path = get_path(file, card);
+int hwmon_isnouveau(char *path) {
   FILE *fp;
   char buffer[pbufsize];
-  fp = fopen(path, "r");
+  char *nv = "nouveau";
+  int rval = 0;
+  char *filep = combine(path, "/name");
+
+  fp = fopen(filep, "r");
 
   if(!fp) {
-    fprintf(stderr, "Error while opening: %s\n", path);
+    fprintf(stderr, "Error while opening: %s\n", filep);
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  if(fgets(buffer, pbufsize, fp) != NULL) {
+    if(strncmp(buffer, nv, strlen(nv)) == 0) {
+        rval = 1;
+    }
+  }
+
+  safefree((void **)&filep);
+  fclose(fp);
+  return rval;
+}
+
+char *get_hwmon() {
+  char *hwmon_p = NULL;
+  DIR *dp;
+  struct dirent *ep;
+  dp = opendir(HWMON_PATH);
+
+  if(dp != NULL) {
+    while((ep = readdir(dp))) {
+      if(strcmp(ep->d_name, ".") != 0 && strcmp(ep->d_name, "..") != 0) {
+        DIR *cdp;
+        struct dirent *cep;
+        char *path = combine(HWMON_PATH, ep->d_name);
+        cdp = opendir(path);
+
+        if(cdp != NULL) {
+          while((cep = readdir(cdp))) {
+            if(strcmp(cep->d_name, "name") == 0) {
+              if(hwmon_isnouveau(path)) {
+                hwmon_p = malloc(strlen(path) + strlen(ep->d_name) + 1);
+                strcpy(hwmon_p, ep->d_name);
+                break;
+              }
+            }
+          }
+        }
+
+        closedir(cdp);
+        safefree((void **)&path);
+        if(hwmon_p != NULL)
+          break;
+      }
+    }
+
+    closedir(dp);
+  }
+  else {
+    fprintf(stderr, "Couldnt open the directory : %s \n", HWMON_PATH);
+  }
+
+  if(hwmon_p == NULL)
+    hwmon_p = "Unknown";
+
+  return hwmon_p;
+}
+
+int get_temp(char *path) {
+  FILE *fp;
+  char buffer[pbufsize];
+  int rval = 0;
+  char *filep = combine(path, HWMON_TEMP_FILE);
+
+  fp = fopen(filep, "r");
+
+  if(!fp) {
+    fprintf(stderr, "Error while opening: %s\n", filep);
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  if(fgets(buffer, pbufsize, fp) != NULL) {
+    rval = strtol(buffer, (char **)NULL, 10);
+    rval = rval / 1000;
+  }
+
+  safefree((void **)&filep);
+  fclose(fp);
+  return rval;
+}
+
+char *get_clk(char *path) {
+  FILE *fp;
+  char buffer[pbufsize] = {0};
+  char *filep = combine(path, PSTATE_FILE);
+  char *acline = "AC: ";
+  char *dcline = "DC: ";
+  char *unkline = "--: ";
+  char *str = NULL;
+
+  size_t len = strlen(acline); //only needs one len, same amount of chars on all three.
+
+  fp = fopen(filep, "r");
+
+  if(!fp) {
+    fprintf(stderr, "Error while opening: %s\n", filep);
+    fprintf(stderr, "%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  while(fgets(buffer, pbufsize, fp) != NULL) {
+    if(strncmp(acline, buffer, len) == 0 || strncmp(dcline, buffer, len) == 0 || strncmp(unkline, buffer, len) == 0) {
+      char line[pbufsize];
+      for(int i = len; i < pbufsize; i++) {
+        if(buffer[i] == '\n')
+          line[i-len] = '\0';
+        else
+          line[i-len] = buffer[i];
+      }
+
+      size_t l = strlen(line) + 1;
+      str = malloc(l);
+      strcpy(str, line);
+    }
+  }
+
+  safefree((void **)&filep);
+  fclose(fp);
+
+  return str;
+}
+
+void print_file(char *file) {
+  FILE *fp;
+  char buffer[pbufsize];
+  fp = fopen(file, "r");
+
+  if(!fp) {
+    fprintf(stderr, "Error while opening: %s\n", file);
     fprintf(stderr, "%s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
@@ -119,18 +264,15 @@ void print_file(char *file, char *card) {
   }
 
   fclose(fp);
-  fp = NULL;
-  safefree((void **)&path);
 }
 
-void set_val(char *file, char *card, char *val) {
-  char *path = get_path(file, card);
+void write_tofile(char *file, char *val) {
   FILE *fp;
 
-  fp = fopen(path, "w");
+  fp = fopen(file, "w");
 
   if(!fp) {
-    fprintf(stderr, "Error while opening: %s\n", path);
+    fprintf(stderr, "Error while opening: %s\n", file);
     fprintf(stderr, "%s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
@@ -138,40 +280,57 @@ void set_val(char *file, char *card, char *val) {
   fprintf(fp, "%s\n", val);
 
   fclose(fp);
-  fp = NULL;
-  safefree((void **)&path);
 }
 
-void print_clocks(char *file, char *card) {
-  char *path = get_path(file, card);
-  FILE *fp;
-  char buffer[pbufsize];
-  fp = fopen(path, "r");
+void fill_struct(struct nouveau *nv, char *c) {
+  char *cards;
 
-  if(!fp) {
-    fprintf(stderr, "Error while opening: %s\n", path);
-    fprintf(stderr, "%s\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+  if(c)
+    cards = c;
+  else
+    cards = get_cards(0);
 
-  char *acline = "AC: ";
-  char *dcline = "DC: ";
-  char *unkline ="--: ";
-  size_t len = strlen(acline); //only needs one len, same amount of chars on all three.
+  char *hwmon = get_hwmon();
 
-  while(fgets(buffer, pbufsize, fp) != NULL) {
-    if(strncmp(acline, buffer, len) == 0 || strncmp(dcline, buffer, len) == 0 || strncmp(unkline, buffer, len) == 0) {
-      char line[pbufsize];
-      for(int i = len; i < pbufsize; i++)
-        line[i-len] = buffer[i];
+  nv->dri_path = combine(DRI_PATH, cards);
+  nv->hwmon_path = combine(HWMON_PATH, hwmon);
+  nv->pstate_file = combine(nv->dri_path, PSTATE_FILE);
+  nv->boost_file = combine(nv->dri_path, BOOST_FILE);
+  nv->gpu_temp = get_temp(nv->hwmon_path);
+  nv->current_clocks = get_clk(nv->dri_path);
 
-      fprintf(stdout, "%s", line);
-    }
-  }
+  safefree((void **)&cards);
+  safefree((void **)&hwmon);
+}
 
-  fclose(fp);
-  fp = NULL;
-  safefree((void **)&path);
+void handle_opts(struct optargs arg, struct nouveau nv) {
+  if(arg.pstate)
+    write_tofile(nv.pstate_file, arg.pstate);
+  if(arg.boost)
+    write_tofile(nv.boost_file, arg.boost);
+  if(arg.printclocks)
+    fprintf(stdout, "%s \n", nv.current_clocks);
+  if(arg.printpstate)
+    print_file(nv.pstate_file);
+  if(arg.printboost)
+    print_file(nv.boost_file);
+  if(arg.printcards)
+    get_cards(1);
+  if(arg.printtemp)
+    fprintf(stdout, "%i°C \n", nv.gpu_temp);
+}
+
+void free_struct(struct nouveau *nv) {
+  if(nv->dri_path)
+    safefree((void **)&nv->dri_path);
+  if(nv->hwmon_path)
+    safefree((void **)&nv->hwmon_path);
+  if(nv->pstate_file)
+    safefree((void **)&nv->pstate_file);
+  if(nv->boost_file)
+    safefree((void **)&nv->boost_file);
+  if(nv->current_clocks)
+    safefree((void **)&nv->current_clocks);
 }
 
 void print_help() {
@@ -184,6 +343,7 @@ void print_help() {
   fprintf(stdout, "  -pVALUE, --pstate=VALUE    set pstate to VALUE, if no VALUE is provided print the file.\n");
   fprintf(stdout, "  -g, --get-clocks           print 'core {HZ} MHz memory {HZ} MHz' of current pstate. to make things\n");
   fprintf(stdout, "                              easier for bash scripts parsing.\n\n");
+  fprintf(stdout, "  -t, --get-temp             print gpu temp in °C.\n");
   fprintf(stdout, "  -h, --help                 print this help and exit\n\n");
   exit(EXIT_SUCCESS);
 }
@@ -194,65 +354,74 @@ int main(int argc, char **argv) {
     {"card", optional_argument, 0, 'c'},
     {"pstate", optional_argument, 0, 'p'},
     {"get-clocks", no_argument, 0, 'g'},
+    {"get-temp", no_argument, 0, 't'},
     {"help", no_argument, 0, 'h'},
     {0, 0, 0, 0}
   };
 
-  int printcards = 0;
-  int printboost = 0;
-  int printpstate = 0;
-  int printclocks = 0;
-  int alloccard = 0;
-  char *cardarg = NULL;
-  char *boostarg = NULL;
-  char *pstatearg = NULL;
+  struct optargs arg;
+  arg.pstate = NULL;
+  arg.boost = NULL;
+  arg.card = NULL;
+  arg.printclocks = 0;
+  arg.printpstate = 0;
+  arg.printboost = 0;
+  arg.printcards = 0;
+  arg.printtemp = 0;
 
   int option_index = 0;
   int opt;
 
-  while((opt = getopt_long(argc, argv, "c::b::p::gh", long_opt, &option_index)) != -1) {
+  while((opt = getopt_long(argc, argv, "c::b::p::gth", long_opt, &option_index)) != -1) {
     switch(opt) {
       case 'b':
         if(optarg) {
-          boostarg = optarg;
+          arg.boost = optarg;
         }
         else {
-          printboost = 1;
+          arg.printboost = 1;
         }
       break;
 
       case 'c':
         if(optarg) {
-          cardarg = optarg;
+          arg.card = optarg;
         }
         else {
-          printcards = 1;
+          arg.printcards = 1;
         }
       break;
 
       case 'p':
         if(optarg) {
-          pstatearg = optarg;
+          arg.pstate = optarg;
         }
         else {
-          printpstate = 1;
+          arg.printpstate = 1;
         }
       break;
 
       case 'g':
-        printclocks = 1;
+        arg.printclocks = 1;
+      break;
+
+      case 't':
+        arg.printtemp = 1;
+      break;
+
+      case '?':
+      case '0':
+        print_help();
       break;
 
       default:
         print_help();
-        exit(EXIT_SUCCESS);
       break;
     }
   }
 
   if(argc == 1) {
     print_help();
-    exit(EXIT_SUCCESS);
   }
 
   if(geteuid() != 0) {
@@ -260,41 +429,11 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  if(cardarg == NULL) {
-    cardarg = get_cards(0);
-    alloccard = 1;
-  }
+  struct nouveau nv;
+  fill_struct(&nv, arg.card);
+  handle_opts(arg, nv);
 
-  if(printboost) {
-    fprintf(stdout, "Boost Entrys on DRI card: %s\n", cardarg);
-    print_file(BOOST_FILE, cardarg);
-  }
-  else if(boostarg != NULL) {
-    set_val(BOOST_FILE, cardarg, boostarg);
-  }
-
-  if(printpstate) {
-    fprintf(stdout, "Pstate Entrys on DRI card: %s\n", cardarg);
-    print_file(PSTATE_FILE, cardarg);
-  }
-  else if(pstatearg != NULL)
-  {
-    set_val(PSTATE_FILE, cardarg, pstatearg);
-  }
-
-  if(printcards) {
-    fprintf(stdout, "Available DRI cards: \n");
-    get_cards(1);
-  }
-
-  if(printclocks) {
-    print_clocks(PSTATE_FILE, cardarg);
-  }
-
-  if(alloccard) {
-    safefree((void **)&cardarg);
-  }
-
+  free_struct(&nv);
   return EXIT_SUCCESS;
 }
 
